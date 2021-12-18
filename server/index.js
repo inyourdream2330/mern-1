07950a2a77f1eb58afd7b672cd9177457bfc1266
Cheems <inyourdream2330@gmail.com>
2021-12-18 09:41:35 +0700

    add engines

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+
 const app = express();
 app.use(express.json());
 
@@ -10,7 +11,7 @@ const postRouter = require("./routes/post.js");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mern-1.whueg.mongodb.net/mern-1?retryWrites=true&w=majority`,
+      `mongodb+srv://$root:12345@mern-1.whueg.mongodb.net/mern-1?retryWrites=true&w=majority`,
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,4 +1,3 @@
-require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const veryfyToken = (req, res, next) => {
   const authHeader = req.header("Authorization");
