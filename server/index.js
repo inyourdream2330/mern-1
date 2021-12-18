108a7818e89d55c036388fedd1eefef5330e4b2f
Cheems <inyourdream2330@gmail.com>
2021-12-18 10:25:09 +0700

    add engines

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-require("dotenv").config({ path: "./.env" });
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -25,18 +25,7 @@ const connectDB = async () => {
 connectDB();
 
 app.use(express.json());
-const whitelist = ["http://localhost:3000"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
