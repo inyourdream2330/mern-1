4634b39b805750f2e8a5ce7c335e9137312fc11f
Cheems <inyourdream2330@gmail.com>
2021-12-18 20:51:16 +0700

    fix cors

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
 
 const authRouter = require("./routes/auth.js");
 const postRouter = require("./routes/post.js");
@@ -23,12 +22,18 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
-
-connectDB();
-app.get("/", (req, res) => {
-  console.log("fixed cors");
-  res.set("Access-Control-Allow-Origin", "*");
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
 });
+connectDB();
+
 app.use("/api/auth", authRouter);
 app.use("/api/posts", postRouter);
 
